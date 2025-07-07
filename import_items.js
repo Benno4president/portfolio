36f4d5d8ec0ff5be594f14635656de1b1420ac49
bueno <bueno@bb.b>
2025-07-07 17:04:28 +0200

    is baking a better idea?

--- import_items.js
+++ import_items.js
@@ -1,9 +1,14 @@
 function importAndAppendItems() {
-  fetch(
+  
+    fetch("https://raw.githubusercontent.com/Benno4president/portfolio/refs/heads/main/items/test.html")
+        .then((res) => {res.text().then((text) => {console.log(text)})})
+    return
+    fetch(
     "https://api.github.com/repos/Benno4president/portfolio/contents/items/test.html"
   )
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
+      const decodedContent = atob(data.content.replace(/\n/g, ""));
+      console.log(decodedContent);
     });
 }
